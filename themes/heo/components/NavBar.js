9471453cece90de130fe6173ca1941221d03b3b2
RocketLu <skywelllwq@gmail.com>
2024-01-11 17:00:12 +0800

    Update NavBar.js

--- themes/heo/components/NavBar.js
+++ themes/heo/components/NavBar.js
@@ -145,7 +145,7 @@ const NavBar = props => {
 
                 {/* 右侧固定 */}
                 <div className='flex flex-shrink-0 justify-center items-center'>
-                    <RandomPostButton {...props} />
+                 {/* <RandomPostButton {...props} />*/}
                     <SearchButton {...props}/>
                     {!JSON.parse(siteConfig('THEME_SWITCH')) && <div className='hidden md:block'><DarkModeButton {...props} /></div>}
                     <ReadingProgress />
