4c5b9694866270c71f8a9ebdabc4333840e8f24e
RocketLu <skywelllwq@gmail.com>
2024-01-11 18:20:32 +0800

    Update SideRight.js

--- themes/heo/components/SideRight.js
+++ themes/heo/components/SideRight.js
@@ -62,7 +62,7 @@ export default function SideRight(props) {
                     <TagGroups tags={tagOptions} currentTag={currentTag} />
                     <hr className='mx-1 flex border-dashed relative my-4' />
                     <AnalyticsCard {...props} />
-                </Card>
+                </Card> */}
             </div>
 
         </div>
