0cd49327d54ed5a96e9d30fd88bb96f19c406628
RocketLu <skywelllwq@gmail.com>
2024-01-12 10:44:25 +0800

    Update TouchMeCard.js

--- themes/heo/components/TouchMeCard.js
+++ themes/heo/components/TouchMeCard.js
@@ -19,7 +19,7 @@ export default function TouchMeCard() {
                 className='cursor-pointer lg:p-6 p-4 border rounded-l bg-[#4f65f0] dark:bg-blue-600 dark:border-gray-600'
                 frontContent={
                     <div className='h-full'>
-                        <h2 className='font-[1000] text-2xl'>{siteConfig('HEO_SOCIAL_CARD_TITLE_1', null, CONFIG)}</h2>
+                        <h2 className='font-[1000] text-xl'>{siteConfig('HEO_SOCIAL_CARD_TITLE_1', null, CONFIG)}</h2>
                         <h3 className='pt-2'>{siteConfig('HEO_SOCIAL_CARD_TITLE_2', null, CONFIG)}</h3>
                         <div className='absolute left-0 top-0 w-full h-full' style={{ background: 'url(https://bu.dusays.com/2023/05/16/64633c4cd36a9.png) center center no-repeat' }}></div>
                     </div>}
