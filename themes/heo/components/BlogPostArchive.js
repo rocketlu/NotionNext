a75e58c03d6817028fc3436b29b44c71663bb484
RocketLu <skywelllwq@gmail.com>
2024-01-12 10:31:15 +0800

    Update BlogPostArchive.js

--- themes/heo/components/BlogPostArchive.js
+++ themes/heo/components/BlogPostArchive.js
@@ -29,13 +29,13 @@ const BlogPostArchive = ({ posts = [], archiveTitle, siteInfo }) => {
                         post.pageCoverThumbnail = siteInfo?.pageCover
                       }
                       const showPageCover = siteConfig('HEO_POST_LIST_COVER', null, CONFIG) && post?.pageCoverThumbnail && !showPreview
-                      return <div key={post.id} className={'cursor-pointer flex flex-row mb-4 h-24 md:flex-row group w-full  dark:border-gray-600 hover:border-indigo-600  dark:hover:border-yellow-600 duration-300 transition-colors justify-between overflow-hidden'}>
+                      return <div key={post.id} className={'cursor-pointer flex flex-row mb-4 h-24 md:flex-row group w-full  dark:border-gray-600 hover:border-indigo-600  dark:hover:border-indigo-600 duration-300 transition-colors justify-between overflow-hidden'}>
 
                             {/* 图片封面 */}
                             {showPageCover && (
                                 <div>
                                     <Link href={`${siteConfig('SUB_PATH', '')}/${post.slug}`} passHref legacyBehavior>
-                                        <LazyImage className={'rounded-xl bg-center bg-cover w-40 h-24'} src={post?.pageCoverThumbnail}/>
+                                        <LazyImage className={'rounded-l bg-center bg-cover w-40 h-24'} src={post?.pageCoverThumbnail}/>
                                     </Link>
                                 </div>
                             )}
@@ -55,7 +55,7 @@ const BlogPostArchive = ({ posts = [], archiveTitle, siteInfo }) => {
                                     <Link
                                         href={`${siteConfig('SUB_PATH', '')}/${post.slug}`}
                                         passHref
-                                        className={' group-hover:text-indigo-700 group-hover:dark:text-indigo-400 text-black dark:text-gray-100 dark:group-hover:text-yellow-600 line-clamp-2 replace cursor-pointer text-xl font-extrabold leading-tight'}>
+                                        className={' group-hover:text-indigo-700 group-hover:dark:text-indigo-400 text-black dark:text-gray-100 dark:group-hover:text-indigo-600 line-clamp-2 replace cursor-pointer text-xl font-extrabold leading-tight'}>
                                         <span className='menu-link '>{post.title}</span>
                                     </Link>
                                 </div>
