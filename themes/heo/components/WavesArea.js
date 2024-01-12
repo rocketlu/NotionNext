2c301e25932179b203978758eeb45f60f577a3b4
RocketLu <skywelllwq@gmail.com>
2024-01-12 10:48:38 +0800

    Update WavesArea.js

--- themes/heo/components/WavesArea.js
+++ themes/heo/components/WavesArea.js
@@ -9,15 +9,15 @@ export default function WavesArea() {
 
   return (
         <section className="main-hero-waves-area waves-area w-full absolute left-0 z-10 bottom-0">
-            <svg className="waves-svg w-full h-[60px]" xmlns="http://www.w3.org/2000/svg" xlink="http://www.w3.org/1999/xlink" viewBox="0 24 150 28" preserveAspectRatio="none" shapeRendering="auto">
+            <svg className="waves-svg w-full h-[40px]" xmlns="http://www.w3.org/2000/svg" xlink="http://www.w3.org/1999/xlink" viewBox="0 24 150 28" preserveAspectRatio="none" shapeRendering="auto">
                 <defs>
                     <path id="gentle-wave" d="M -160 44 c 30 0 58 -18 88 -18 s 58 18 88 18 s 58 -18 88 -18 s 58 18 88 18 v 44 h -352 Z"></path>
                 </defs>
                 <g className="parallax">
                     <use href="#gentle-wave" x="48" y="0"></use>
+                   /* <use href="#gentle-wave" x="48" y="0"></use>
                     <use href="#gentle-wave" x="48" y="0"></use>
-                    <use href="#gentle-wave" x="48" y="0"></use>
-                    <use href="#gentle-wave" x="48" y="0"></use>
+                    <use href="#gentle-wave" x="48" y="0"></use> */
                 </g>
             </svg>
             <style jsx global>{`
@@ -32,7 +32,7 @@ export default function WavesArea() {
                   fill: ${color};
                   opacity: 0.5;
                 }
-                .parallax > use:nth-child(2) {
+                /* .parallax > use:nth-child(2) {
                   animation-delay: -3s;
                   animation-duration: 10s;
                   fill: ${color};
@@ -48,7 +48,7 @@ export default function WavesArea() {
                   animation-delay: -5s;
                   animation-duration: 20s;
                   fill: ${color};
-                }
+                }*/
                 
                 @keyframes move-forever {
                   0% {
