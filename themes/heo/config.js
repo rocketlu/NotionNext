a9c13991db369919443d685701023a6002e70bc4
RocketLu <skywelllwq@gmail.com>
2026-03-06 14:01:47 +0800

    Update configuration for site and hero section

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -7,10 +7,10 @@ const CONFIG = {
   HEO_SITE_CREATE_TIME: '2021-09-21', // 建站日期，用于计算网站运行的第几天
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
-  HEO_NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
-    { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
-  ],
+ // HEO_NOTICE_BAR: [
+ // { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
+ // { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
+// ],
 
   // 英雄区左右侧组件颠倒位置
   HEO_HERO_REVERSE: false,
@@ -18,19 +18,19 @@ const CONFIG = {
   HEO_HERO_BODY_REVERSE: false,
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '分享编程',
-  HEO_HERO_TITLE_2: '与思维认知',
-  HEO_HERO_TITLE_3: 'TANGLY1024.COM',
-  HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
+  HEO_HERO_TITLE_1: '一起探索',
+  HEO_HERO_TITLE_2: 'AIGC/ART/DATAVIS',
+  HEO_HERO_TITLE_3: 'Rocketlu',
+  HEO_HERO_TITLE_4: '关注Rocket',
+  HEO_HERO_TITLE_5: '如何1分钟拥有自己的Avatar',
+  HEO_HERO_TITLE_LINK: 'https://chat.openai.com/g/g-4K5QtDnBn-avatars-creator',
   // 英雄区遮罩文字
   HEO_HERO_COVER_TITLE: '随便逛逛',
 
   // 英雄区显示三个置顶分类
-  HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
-  HEO_HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
-  HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
+  HEO_HERO_CATEGORY_1: { title: 'AIGC', url: '/tag/AIGC' },
+  HEO_HERO_CATEGORY_2: { title: 'ART', url: '/tag/Art' },
+  HEO_HERO_CATEGORY_3: { title: 'DATAVIS', url: '/tag/DataVis' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
@@ -39,7 +39,7 @@ const CONFIG = {
 
   // 右侧个人资料卡牌欢迎语，点击可自动切换
   HEO_INFOCARD_GREETINGS: [
-    '你好！我是',
+    '👋🏻 Hi！我是Rocket',
     '🔍 分享与热心帮助',
     '🤝 专修交互与设计',
     '🏃 脚踏实地行动派',
@@ -51,9 +51,9 @@ const CONFIG = {
   // 个人资料底部按钮
   HEO_INFO_CARD_URL1: '/about',
   HEO_INFO_CARD_ICON1: 'fas fa-user',
-  HEO_INFO_CARD_URL2: 'https://github.com/tangly1024',
+  HEO_INFO_CARD_URL2: 'https://github.com/rocketlu',
   HEO_INFO_CARD_ICON2: 'fab fa-github',
-  HEO_INFO_CARD_URL3: 'https://www.tangly1024.com',
+  HEO_INFO_CARD_URL3: 'https://github.com/rocketlu',
   HEO_INFO_CARD_TEXT3: '了解更多',
 
   // 用户技能图标
@@ -63,13 +63,13 @@ const CONFIG = {
       img_1: '/images/heo/20239df3f66615b532ce571eac6d14ff21cf072602.webp',
       color_1: '#989bf8',
       title_2: 'Sketch',
-      img_2: '/images/heo/2023e0ded7b724a39f12d59c3dc8fbdc7cbe074202.webp',
-      color_2: '#ffffff'
+      img_2: '/images/heo/mj.png',
+      color_2: '#242425'
     },
     {
       title_1: 'Docker',
-      img_1: '/images/heo/20231108a540b2862d26f8850172e4ea58ed075102.webp',
-      color_1: '#57b6e6',
+      img_1: '/images/heo/sd.png',
+      color_1: '#9E43FF',
       title_2: 'Photoshop',
       img_2: '/images/heo/2023e4058a91608ea41751c4f102b131f267075902.webp',
       color_2: '#4082c3'
@@ -84,43 +84,43 @@ const CONFIG = {
     },
     {
       title_1: 'Swift',
-      img_1: '/images/heo/202328bbee0b314297917b327df4a704db5c072402.webp',
-      color_1: '#eb6840',
+      img_1: '/images/heo/gpt.png',
+      color_1: '#297260',
       title_2: 'Principle',
-      img_2: '/images/heo/2023f76570d2770c8e84801f7e107cd911b5073202.webp',
-      color_2: '#8f55ba'
+      img_2: '/images/heo/hf.png',
+      color_2: '#FFC700'
     },
     {
       title_1: 'illustrator',
       img_1: '/images/heo/20237359d71b45ab77829cee5972e36f8c30073902.webp',
       color_1: '#f29e39',
       title_2: 'CSS3',
-      img_2: '/images/heo/20237c548846044a20dad68a13c0f0e1502f074602.webp',
-      color_2: '#2c51db'
+      img_2: '/images/heo/ci.png',
+      color_2: '#ffffff'
     },
     {
       title_1: 'JS',
       img_1: '/images/heo/2023786e7fc488f453d5fb2be760c96185c0075502.webp',
       color_1: '#f7cb4f',
       title_2: 'HTML',
-      img_2: '/images/heo/202372b4d760fd8a497d442140c295655426070302.webp',
-      color_2: '#e9572b'
+      img_2: '/images/heo/rw.png',
+      color_2: '#ffffff'
     },
     {
       title_1: 'Git',
       img_1: '/images/heo/2023ffa5707c4e25b6beb3e6a3d286ede4c6071102.webp',
       color_1: '#df5b40',
       title_2: 'Rhino',
-      img_2: '/images/heo/20231ca53fa0b09a3ff1df89acd7515e9516173302.webp',
-      color_2: '#1f1f1f'
+      img_2: '/images/heo/aifunr.png',
+      color_2: '#ffffff'
     }
   ],
-
+  
   HEO_SOCIAL_CARD: true, // 是否显示右侧，点击加入社群按钮
-  HEO_SOCIAL_CARD_TITLE_1: '交流频道',
-  HEO_SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享',
-  HEO_SOCIAL_CARD_TITLE_3: '点击加入社群',
-  HEO_SOCIAL_CARD_URL: 'https://docs.tangly1024.com/article/how-to-question',
+  HEO_SOCIAL_CARD_TITLE_1: 'AIGC创作者社区',
+  HEO_SOCIAL_CARD_TITLE_2: '加入智否AIFunr社区讨论分享',
+  HEO_SOCIAL_CARD_TITLE_3: '🚀 点击加入智否',
+  HEO_SOCIAL_CARD_URL: 'https://aifunr.com',
 
   // 底部统计面板文案
   HEO_POST_COUNT_TITLE: '文章数:',
